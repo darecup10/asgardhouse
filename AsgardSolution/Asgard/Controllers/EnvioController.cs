661551930ecbbad2730ea143230dae6c10f14574
T11641.Training40 <T11641.Training40@accenture.com>
2019-07-19 19:08:19 -0300

    Create MVC

---
+++ AsgardSolution/Asgard/Controllers/EnvioController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Asgard.Models;
+
+namespace Asgard.Controllers
+{
+    public class EnvioController : Controller
+    {
+        private AsgardEntities1 db = new AsgardEntities1();
+
+        // GET: Envio
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Include(o => o.User);
+            return View(orders.ToList());
+        }
+
+        // GET: Envio/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Envio/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.Users, "DNI", "Name");
+            return View();
+        }
+
+        // POST: Envio/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserID,Date,Adress")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Orders.Add(order);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.Users, "DNI", "Name", order.UserID);
+            return View(order);
+        }
+
+        // GET: Envio/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.Users, "DNI", "Name", order.UserID);
+            return View(order);
+        }
+
+        // POST: Envio/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserID,Date,Adress")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.Users, "DNI", "Name", order.UserID);
+            return View(order);
+        }
+
+        // GET: Envio/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Order order = db.Orders.Find(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Envio/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Order order = db.Orders.Find(id);
+            db.Orders.Remove(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AsgardSolution/Asgard/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asgard.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.AsgardEntities1 db = new Models.AsgardEntities1();
+            //linq devolver informacion
+            var List = from o in db.Orders
+                       select o;
+            return View(List.ToList());
+        }
+        //metodo de accion create get
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Models.Order order = new Models.Order();
+
+            return View(order);
+        }
+        [HttpPost]
+        public ActionResult Create(Models.Order order)
+        {
+            Models.AsgardEntities1 db = new Models.AsgardEntities1();
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
